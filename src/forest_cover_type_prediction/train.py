503b33b2aeb22de6f25de162c0b09e6c6977414d
Mikhail Lukashevich <lukashevichmikhail@gmail.com>
2022-05-11 13:11:46 +0300

    Add test split ratio CLI option

--- src/forest_cover_type_prediction/train.py
+++ src/forest_cover_type_prediction/train.py
@@ -14,14 +14,18 @@ from sklearn.model_selection import train_test_split
     default="data/forest_cover.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
-@click.option("--random-state", default=42, type=int)
+@click.option(
+    "--test-split-ratio",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+)
 def train(dataset_path: Path, random_state: int) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop(labels=['Id','Cover_Type'],axis=1)
     target = dataset['Cover_Type']
     features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=0.2, random_state=random_state
+        features, target, test_size=train_test_split, random_state=random_state
     )
     classifier = LogisticRegression(random_state=random_state).fit(
         features_train, target_train
