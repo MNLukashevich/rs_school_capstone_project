12f3bb0be0d644839340072baeea2c52a1b34129
Mikhail Lukashevich <lukashevichmikhail@gmail.com>
2022-05-11 13:24:46 +0300

    Extract dataset loading to a separate module

--- src/forest_cover_type_prediction/train.py
+++ src/forest_cover_type_prediction/train.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
 import click
-import pandas as pd
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 
+from .data import get_dataset
 from .pipeline import create_pipeline
 
 @click.command()
@@ -19,13 +18,11 @@ from .pipeline import create_pipeline
     default=0.2,
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
 )
-def train(dataset_path: Path, random_state: int) -> None:
-    dataset = pd.read_csv(dataset_path)
-    click.echo(f"Dataset shape: {dataset.shape}.")
-    features = dataset.drop(labels=['Id','Cover_Type'],axis=1)
-    target = dataset['Cover_Type']
-    features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=train_test_split, random_state=random_state
+def train(dataset_path: Path, random_state: int, test_split_ratio: float) -> None:
+    features_train, features_val, target_train, target_val = get_dataset(
+        dataset_path,
+        random_state,
+        test_split_ratio,
     )
     pipeline = create_pipeline(random_state)
     pipeline.fit(features_train, target_train)
